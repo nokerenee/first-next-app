5862857ac112022894d52ef43fcae414b856cea8
Nicole Kelly <nrkelly57@gmail.com>
2023-12-13 22:09:43 +1300

    error page format and drop-down box

--- first-next-app/src/components/PostsLimit.jsx
+++ first-next-app/src/components/PostsLimit.jsx
@@ -1,33 +1,49 @@
 "use client";
-// save as components/PostsLimit.jsx
 
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 // Client component allowing user to choose the number of posts displayed
 // and set new value in search params
-export default function PostsLimit(defaultLimit) {
-  const searchParams = useSearchParams(); // next.js hook for search params
-  const router = useRouter(); // next.js hook for client side navigation
-  const pathname = usePathname(); // next.js hook for current URL path
+export default function PostsLimit({ defaultLimit, defaultPage }) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
   const limit = searchParams.has("limit")
     ? searchParams.get("limit")
     : defaultLimit;
+  const page = searchParams.has("page")
+    ? searchParams.get("page")
+    : defaultPage;
 
   const handleChangeLimit = (e) => {
     // change the route to the existing path plus the new search param
-    router.replace(pathname + "?limit=" + e.target.value);
+    router.replace(`${pathname}?limit=${e.target.value}&page=${page}`);
+  };
+
+  const handleChangePage = (e) => {
+    router.replace(`${pathname}?limit=${limit}&page=${e.target.value}`);
   };
 
   return (
-    <label className="PostsLimit">
-      Number of posts:
-      <select onChange={handleChangeLimit} value={limit}>
-        <option>5</option>
-        <option>10</option>
-        <option>20</option>
-      </select>
-    </label>
+    <div className="PostsLimit">
+      <label>
+        Number of posts:
+        <select onChange={handleChangeLimit} value={limit}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </label>
+      <label>
+        Current Page:
+        <select onChange={handleChangePage} value={page}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          {/* Add more options as needed */}
+        </select>
+      </label>
+    </div>
   );
 }
-// See next slide for changes to server-side Posts component
